bbe4e46bbc656a6f1578941821a39fc9f90639ed
MvdZon <matthijszon@hotmail.com>
2025-08-13 22:00:33 +0200

    reverted some checks for production builds

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ import logClickDirective from './log-click-directive.js'
 import changeDirective from './change-directive.js'
 
 export default {
-    install(Vue, options = {}) {
-        console.log((999))
-        const noop = {}
-        const isProd =
-            (typeof process !== 'undefined' && process.env?.NODE_ENV === 'production') ||
-            (typeof import.meta !== 'undefined' && import.meta.env?.PROD) ||
-            false
-        const directivesEnabled = options.enableInProduction ?? !isProd
-
-        Vue.directive('log', directivesEnabled ? logDirective : noop)
-        Vue.directive('log-click',  directivesEnabled ? logClickDirective : noop)
-        Vue.directive('log-change', directivesEnabled ? changeDirective : noop)
+    install(Vue) {
+        Vue.directive('log', logDirective)
+        Vue.directive('log-click',  logClickDirective)
+        Vue.directive('log-change', changeDirective)
     }
 }
